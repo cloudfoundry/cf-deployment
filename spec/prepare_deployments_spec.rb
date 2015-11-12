e1d4c6d6c0b9417e1ce6c84966eada56dd3d1757
Marco Voelz <marco.voelz@sap.com>
2015-11-12 09:41:33 +0100

    Add sha1 to etcd release when not using 'director-latest'
    Not done in this change: When local .tgz can be given as
    the etcd release path, you also don't need a sha1.
    
    [fixes #6]

--- spec/prepare_deployments_spec.rb
+++ spec/prepare_deployments_spec.rb
@@ -651,6 +651,24 @@ HEREDOC
     end
     describe 'print_releases_stub' do
       context 'everything is provided' do
+        let(:command) { ". ./tools/prepare-deployments && print_releases_stub 1 banana 1 banana 'sha1: 1234edf'" }
+        it 'prints stemcell stub' do
+          expect(result).to be_success
+          expect(stdout).to eq(<<HEREDOC
+---
+releases:
+  - name: cf
+    version: 1
+    url: banana
+  - name: etcd
+    version: 1
+    url: banana
+    sha1: 1234edf
+HEREDOC
+)
+        end
+      end
+      context 'no sha1 for etcd is provided' do
         let(:command) { ". ./tools/prepare-deployments && print_releases_stub 1 banana 1 banana" }
         it 'prints stemcell stub' do
           expect(result).to be_success
@@ -663,11 +681,11 @@ releases:
   - name: etcd
     version: 1
     url: banana
+    
 HEREDOC
 )
         end
       end
-
     end
   end
 
