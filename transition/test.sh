f444e7b6008f2794bf1cfd4857202bf6aa20cd05
Jesse Alford <jalford@pivotal.io>
2017-04-04 16:41:43 -0700

    Exit with the `diff` status code in transition tests
    [#135805355]
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- transition/test.sh
+++ transition/test.sh
@@ -3,10 +3,13 @@
 pushd $(dirname $0) > /dev/null
 diff fixture/expected-vars-store.yml <(spiff merge vars-store-template.yml vars-pre-processing-template.yml fixture/source-cf-manifest.yml)
 
-if [ "$?" == "0" ]; then
+status=$?
+if [ "$status" == "0" ]; then
   echo PASS
 else
   echo FAIL
 fi
 
 popd > /dev/null
+
+exit $status
