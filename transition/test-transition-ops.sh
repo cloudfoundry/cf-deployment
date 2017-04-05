bb8c9bd53e2a05b25e9298fcd23001beef369a4c
Joshua Aresty <joshua.aresty@emc.com>
2017-04-05 14:26:28 -0700

    Specify correct directory in test ops script
    [#135805355]
    
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- transition/test-transition-ops.sh
+++ transition/test-transition-ops.sh
@@ -2,14 +2,17 @@
 
 # NB: our test does not assert that the value that we're looking for is not already present
 # in cf-deployment.yml prior to having ops applied.  We believe this is unimportant to us.
+pushd $(dirname $0) > /dev/null
 status=0
 ops_path=transition-ops.yml
 blobstore_user=$(bosh interpolate ../cf-deployment.yml -o $ops_path --path=/instance_groups/name=blobstore/jobs/name=blobstore/properties/blobstore/admin_users/0/username)
+
 if [ "${blobstore_user}" == "(( blobstore_admin_users_username ))" ];then
   echo PASS blobstore_user
 else
   echo FAIL blobstore_user $blobstore_user "!=" "(( blobstore_admin_users_username ))"
   status=1
 fi
+popd > /dev/null
 
 exit $status
