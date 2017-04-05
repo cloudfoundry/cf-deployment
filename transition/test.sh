779e9aa4519073a95e9fb724371091ce06695c26
Joshua Aresty <joshua.aresty@emc.com>
2017-04-05 16:02:02 -0700

    [#135805355] Add cc_bridge_cc_uploader variables
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- transition/test.sh
+++ transition/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd $(dirname $0) > /dev/null
-diff -wB fixture/expected-vars-store.yml <(spiff merge vars-store-template.yml vars-pre-processing-template.yml fixture/source-cf-manifest.yml)
+diff -wB fixture/expected-vars-store.yml <(spiff merge vars-store-template.yml vars-pre-processing-template.yml fixture/source-cf-manifest.yml fixture/source-diego-manifest.yml)
 
 status=$?
 if [ "$status" == "0" ]; then
