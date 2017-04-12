3276ee4260adbcd793acae54a075a01ce5f7f5e6
Chunyi Lyu <clyu@pivotal.io>
2017-04-12 14:12:57 -0700

    Remove unnecessary `grep -v`
    - Also reformat really long line with multi-line hotness.
    
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- transition/test.sh
+++ transition/test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 pushd $(dirname $0) > /dev/null
-diff -wB fixture/expected-vars-store.yml <(spiff merge vars-store-template.yml vars-pre-processing-template.yml fixture/source-cf-manifest.yml fixture/source-diego-manifest.yml | grep -v bosh-will-generate-me)
+diff -wB fixture/expected-vars-store.yml <(spiff merge \
+  vars-store-template.yml \
+  vars-pre-processing-template.yml \
+  fixture/source-cf-manifest.yml \
+  fixture/source-diego-manifest.yml )
 
 status=$?
 if [ "$status" == "0" ]; then
