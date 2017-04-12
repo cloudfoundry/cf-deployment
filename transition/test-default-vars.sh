3f0d2fb7356e36d405a4bc580008231e925ce300
Ming Xiao <mxiao@pivotal.io>
2017-04-12 10:09:37 -0700

    Add test fixture to test for default values
    [#135805355]
    
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

---
+++ transition/test-default-vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pushd $(dirname $0) > /dev/null
+diff -wB fixture/default-values-cf-manifest.yml <(spiff merge vars-store-template.yml vars-pre-processing-template.yml)
+
+status=$?
+if [ "$status" == "0" ]; then
+  echo PASS
+else
+  echo FAIL
+fi
+
+popd > /dev/null
+
+exit $status
