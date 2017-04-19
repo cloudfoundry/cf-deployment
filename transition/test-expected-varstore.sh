192f2faf0a5ece0d2427d3cb858d2166c794d5f2
Chunyi Lyu <clyu@pivotal.io>
2017-04-19 09:02:49 -0700

    Add private keys for all CAs
    - Get service_cf_internal_ca `ca`, `certificate` and `private_key` from diego_ca.
    
    [#143737725]
    
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- transition/test-expected-varstore.sh
+++ transition/test-expected-varstore.sh
@@ -4,8 +4,10 @@ pushd $(dirname $0) > /dev/null
 diff -wB -C5 fixture/expected-vars-store.yml <(spiff merge \
   vars-store-template.yml \
   vars-pre-processing-template.yml \
+  vars-ca-template.yml \
   fixture/source-cf-manifest.yml \
-  fixture/source-diego-manifest.yml )
+  fixture/source-diego-manifest.yml \
+  fixture/ca-private-keys.yml )
 
 status=$?
 if [ "$status" == "0" ]; then
