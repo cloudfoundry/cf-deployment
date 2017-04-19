4c5ff19ae7349665beb3b1ff5c22d93f46216fd5
Steve Taylor <staylor@pivotal.io>
2017-04-19 09:41:59 -0700

    [#143737725] Add tests for required CA keys
    Also, introduce tests-suite.sh, which is a shortcut for running all the
    tests for development purposes.  Contiue to run them separately in CI,
    please.
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

---
+++ transition/test-ca-keys-required.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+fail_required_keys=0
+fail_filling_in=0
+
+spiff merge vars-ca-template.yml 2&> /dev/null
+if [ "$?" == "1" ]; then
+  echo PASS - the ca template requires filling in keys
+else
+  fail_required_keys=1
+  echo FAIL - the ca template requires filling in keys
+fi
+
+spiff merge vars-ca-template.yml fixture/ca-private-keys.yml > /dev/null
+if [ "$?" == "0" ]; then
+  echo PASS - the ca template can be filled completely with keys
+else
+  fail_filling_in=1
+  echo FAIL - the ca template can be filled completely with keys
+fi
+
+if [[ "$fail_required_keys" == "1" || "$fail_filling_in" == "1" ]]; then
+  exit 1
+else
+  exit 0
+fi
---
+++ transition/test-suite.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./test-ca-keys-required.sh
+./test-expected-varstore.sh
+./test-transition-ops.sh
