b774aa571b8dd61c29774783e269d4b910987f25
Steve Taylor <staylor@pivotal.io>
2017-10-24 20:09:02 -0700

    [#152132770] Add missing test opsfiles interpolation test

--- scripts/test-test-ops.sh
+++ scripts/test-test-ops.sh
@@ -6,10 +6,11 @@ test_test_ops() {
 
   pushd ${home} > /dev/null
     pushd operations/test > /dev/null
-      check_interpolation "name: add-datadog-firehose-nozzle.yml" "add-datadog-firehose-nozzle.yml" "-v datadog_api_key=XYZ" "-v datadog_metric_prefix=foo.bar" "-v traffic_controller_external_port=8443"
+      check_interpolation "add-datadog-firehose-nozzle.yml" "-v datadog_api_key=XYZ" "-v datadog_metric_prefix=foo.bar" "-v traffic_controller_external_port=8443"
       check_interpolation "add-persistent-isolation-segment-diego-cell.yml"
       check_interpolation "add-persistent-isolation-segment-router.yml"
       check_interpolation "alter-ssh-proxy-redirect-uri.yml"
+      check_interpolation "name: disable_windows_consul_agent_nameserver_overwriting.yml" "${home}/operations/windows-cell.yml" "-o disable_windows_consul_agent_nameserver_overwriting.yml"
     popd > /dev/null # operations/test
-  popd > /dev/null # operations/test
+  popd > /dev/null
 }
