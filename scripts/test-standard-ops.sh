890dd5db81a688d8f9e19c67efe9f0e4876135cc
Chris Selzo <cselzo@pivotal.io>
2017-10-30 11:59:09 -0700

    [#152301031] Add ops file for setting the bbs active key label
    Signed-off-by: Spencer Hawley <shawley@pivotal.io>

--- scripts/test-standard-ops.sh
+++ scripts/test-standard-ops.sh
@@ -27,6 +27,7 @@ test_standard_ops() {
       check_interpolation "rename-deployment.yml" "-v deployment_name=renamed_deployment"
       check_interpolation "rename-network.yml" "-v network_name=renamed_network"
       check_interpolation "scale-to-one-az.yml"
+      check_interpolation "set-bbs-active-key.yml" "-v diego_bbs_active_key_label=my_key_name"
       check_interpolation "stop-skipping-tls-validation.yml"
       check_interpolation "use-azure-storage-blobstore.yml" "-l example-vars-files/vars-use-azure-storage-blobstore.yml"
       check_interpolation "use-blobstore-cdn.yml" "-l example-vars-files/vars-use-blobstore-cdn.yml"
