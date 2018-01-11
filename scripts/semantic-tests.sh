d2f81e2bcb87a24c6b801fc281c264040cbe392f
Iryna Shustava <ishustava@pivotal.io>
2018-01-11 11:38:58 -0800

    Add semantic tests for scale-to-one-az.yml ops file
    Also, remove a duplicate entry in that ops file. [#154104576]

--- scripts/semantic-tests.sh
+++ scripts/semantic-tests.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-semantic_tests() {
-  # Padded for pretty output
-  suite_name="SEMANTIC    "
-
-  pushd ${home} > /dev/null
+test_rename_network_opsfile() {
     local new_network="test_network"
     local manifest_file=$(mktemp)
 
@@ -22,6 +18,33 @@ semantic_tests() {
     else
       pass "rename-network.yml"
     fi
+}
+
+test_scale_to_one_az() {
+    local manifest_file=$(mktemp)
+
+    bosh int cf-deployment.yml \
+      -o operations/scale-to-one-az.yml > $manifest_file
+
+    local interpolated_instance_counts=$(yq r $manifest_file -j | jq -r .instance_groups[].instances | uniq)
+    local interpolated_azs=$(yq r $manifest_file -j | jq -r .instance_groups[].azs[] | uniq)
+
+    if [ "$interpolated_instance_counts" != "1" ]; then
+      fail "expected all instance counts to be 1"
+    elif [ "$interpolated_azs" != "z1" ]; then
+      fail "expected to find a single AZ for all instance groups"
+    else
+      pass "scale-to-one-az.yml"
+    fi
+}
+
+semantic_tests() {
+  # Padded for pretty output
+  suite_name="SEMANTIC    "
+
+  pushd ${home} > /dev/null
+    test_rename_network_opsfile
+    test_scale_to_one_az
   popd > /dev/null
   exit $exit_code
 }
