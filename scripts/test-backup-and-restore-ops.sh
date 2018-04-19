dc57e1c5a70a4f918809e5545dd2f6aa2b11fa43
Josh Hill <jhill@pivotal.io>
2018-04-19 11:48:52 +0100

    Fix readme and check_interpolation test for BBR nfs
    [#156091150]
    
    Signed-off-by: Rosie Bloxsom <rbloxsom@pivotal.io>

--- scripts/test-backup-and-restore-ops.sh
+++ scripts/test-backup-and-restore-ops.sh
@@ -10,7 +10,7 @@ test_backup_and_restore_ops() {
       check_interpolation "name: enable-backup-restore-credhub.yml" "enable-backup-restore.yml" "-o ${home}/operations/experimental/enable-instance-identity-credentials.yml" "-o ${home}/operations/experimental/secure-service-credentials.yml" "-o enable-backup-restore-credhub.yml"
       check_interpolation "name: enable-backup-restore-s3-versioned.yml" "enable-backup-restore.yml" "-o ${home}/operations/use-s3-blobstore.yml" "-o enable-backup-restore-s3-versioned.yml" "-l ${home}/operations/example-vars-files/vars-use-s3-blobstore.yml"
       check_interpolation "name: enable-backup-restore-s3-unversioned.yml" "enable-backup-restore.yml" "-o ${home}/operations/use-s3-blobstore.yml" "-o enable-backup-restore-s3-unversioned.yml" "-l ${home}/operations/example-vars-files/vars-use-s3-blobstore.yml"
-      check_interpolation "name: enable-nfs-broker-backup.yml" "enable-backup-restore.yml -o enable-nfs-broker-backup.yml -v nfs-broker-database-password=i_am_a_password"
+      check_interpolation "name: enable-backup-restore-nfs-broker.yml" "enable-backup-restore.yml -o enable-backup-restore-nfs-broker.yml -v nfs-broker-database-password=i_am_a_password"
     popd > /dev/null
   popd > /dev/null
   exit $exit_code
