21af4c3c1cf61bf29a1193c8bef27fb3141217ff
Iryna Shustava <ishustava@pivotal.io>
2018-05-11 13:35:51 -0700

    Add perm-service-with-pxc-release.yml to the experimental ops files [#157235412]

--- scripts/test-experimental-ops.sh
+++ scripts/test-experimental-ops.sh
@@ -80,6 +80,7 @@ test_experimental_ops() {
       check_interpolation "name: improve-diego-log-format-windows2016.yml" "windows2016-cell.yml" "-o improve-diego-log-format-windows2016.yml"
       check_interpolation "enable-mysql-tls.yml"
       check_interpolation "name: perm-service.yml" "use-bosh-dns.yml" "-o enable-mysql-tls.yml" "-o perm-service.yml"
+      check_interpolation "name: perm-service-with-pxc-release.yml" "use-bosh-dns.yml" "-o perm-service.yml" "-o use-pxc.yml" "-o perm-service-with-pxc-release.yml"
 
     popd > /dev/null # operations/experimental
   popd > /dev/null
