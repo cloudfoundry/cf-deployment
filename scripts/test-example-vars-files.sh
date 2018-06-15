2a16511ea42f4405b93a4769c52b0e905c5f3f18
Yash Tulsiani <ytulsiani@pivotal.io>
2018-06-15 15:03:33 -0700

    [#158336421] Add tests for example-vars-files to scripts/test
    Signed-off-by: Spencer Hawley <shawley@pivotal.io>

--- scripts/test-example-vars-files.sh
+++ scripts/test-example-vars-files.sh
@@ -2,60 +2,89 @@
 
 test_example_vars_files() {
   set +e
-  # Padded for pretty output
-  suite_name="vars              "
-
-  pushd ${home}/operations/example-vars-files > /dev/null
-    example_vars_files="$(ls)"
-    cf_d_variables="$(yq read ../../cf-deployment.yml -j | jq [.variables[].name])"
-    export VARS=${cf_d_variables}
-
-    for example_vars_file in $example_vars_files; do
-      ops_file="$(echo "${example_vars_file}" | cut -c 6-)"
-      echo $ops_file
-      bosh int "${home}/operations/${ops_file}" -l "${home}/operations/example-vars-files/${example_vars_file}" --var-errs-unused &> /dev/null
-      exit_code=$?
-
-      if [ $exit_code == "0" ]; then
-        bosh int "${home}/operations/${ops_file}" -l "${home}/operations/example-vars-files/${example_vars_file}" --var-errs > /dev/null 2> /tmp/errors.txt
-        exit_code=$?
-
-        if [ $exit_code != "0" ]; then
-          cat /tmp/errors.txt |  grep ":" | wc -l | grep "1" > /dev/null
-          code1=$?
-          cat /tmp/errors.txt |  grep "Expected to find variables:" > /dev/null
-          code2=$?
-
-          if [ $code1 == "0" ] && [ $code2 == "0" ]; then
-            tail -n +2 /tmp/errors.txt  | tail -r | tail -n +2 | cut -c 7- > /tmp/yaml-errors.txt
-            missing_variables="$(yq read /tmp/yaml-errors.txt)"
-
-            for variable in $missing_variables; do
-              length="$(echo $cf_d_variables | jq --arg variable "$variable" 'map(select(. == $variable))' | jq length)"
-
-              if [ $length != "1" ]; then
-                fail "$ops_file"
-              else
-                pass "$ops_file"
-              fi
-            done
-          else
-            fail "$ops_file"
-          fi
-        else
-          pass "$ops_file"
-        fi
-      else
-        fail "$ops_file"
-      fi
-    done
-  popd > /dev/null
+  directories="$(find . -name example-vars-files)"
+  for directory in $directories; do
+    path="$(echo $directory | cut -c 3-)"
+    echo -e "${GREEN} ~~~ Testing ${path} ~~~ ${NOCOLOR}"
+    test_example_vars_files_in_directory $directory
+  done
   set -e
   exit $exit_code
 }
 
-#functionify above
-#restructure pass/fail statements
-#add custom error messages
-#test most edge cases: (eg, no vars-... file exists etc)
+test_example_vars_files_in_directory() {
+  # Padded for pretty output
+  suite_name="VARS              "
+  pushd $directory > /dev/null
+    example_vars_files="$(ls)"
+    cf_d_variables="$(yq read ${home}/cf-deployment.yml -j | jq [.variables[].name])"
+    for example_vars_file in $example_vars_files; do
+      ops_file="$(echo "${example_vars_file}" | cut -c 6-)"
+      if [ ! -f ../${ops_file} ]; then
+        fail "$ops_file does not exist OR ${example_vars_file} does not follow the vars-ops-file-name.yml format"
+      else
+        if [ "$(are_all_present_variables_used)" = "no" ]; then
+          fail "$ops_file failed because it has example variables that are not used"
+        else
+          if [ "$(are_all_needed_variables_present)" = "yes" ]; then
+            pass "$ops_file"
+          else
+            if [ "$(is_it_the_error_we_expect)" = "no" ]; then
+              fail "$ops_file failed because of an unknown bosh error message"
+            else
+              if [ "$(are_missing_variables_in_cf_d)" = "no" ]; then
+                fail "$ops_file failed because not all missing variables are in cf_d"
+              else
+                pass "$ops_file"
+              fi
+            fi
+          fi
+        fi
+      fi
+    done
+  popd > /dev/null
+}
+are_missing_variables_in_cf_d() {
+  tail -n +2 /tmp/errors.txt  | tail -r | tail -n +2 | cut -c 7- > /tmp/yaml-errors.txt
+  missing_variables="$(yq read /tmp/yaml-errors.txt)"
+  for variable in $missing_variables; do
+    length="$(echo $cf_d_variables | jq --arg variable "$variable" 'map(select(. == $variable))' | jq length)"
+    if [ $length != "1" ]; then
+      echo "no"
+    else
+      echo "yes"
+    fi
+  done
+}
 
+is_it_the_error_we_expect() {
+  cat /tmp/errors.txt |  grep ":" | wc -l | grep "       1" > /dev/null
+  code1=$?
+  cat /tmp/errors.txt |  grep "Expected to find variables:" > /dev/null
+  code2=$?
+  if [ $code1 = "0" ] && [ $code2 = "0" ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+are_all_needed_variables_present() {
+  bosh int "../${ops_file}" -l "${example_vars_file}" --var-errs > /dev/null 2> /tmp/errors.txt
+  exit_code=$?
+  if [ $exit_code = "0" ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+are_all_present_variables_used() {
+  bosh int "../${ops_file}" -l "${example_vars_file}" --var-errs-unused &> /dev/null
+  exit_code=$?
+  if [ $exit_code == "0" ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
