f25c4d24b76ba1d8bf0be40bfdd5785ac4c882dd
Dave Walter <dwalter@pivotal.io>
2018-09-19 15:26:05 -0700

    Add new ops-files to test-experimental-ops script
    [#160035537](https://www.pivotaltracker.com/story/show/160035537)

--- scripts/test-experimental-ops.sh
+++ scripts/test-experimental-ops.sh
@@ -25,6 +25,7 @@ test_experimental_ops() {
       check_interpolation "enable-bpm.yml"
       check_interpolation "enable-iptables-logger.yml"
       check_interpolation "enable-mysql-tls.yml"
+      check_interpolation "enable-nfs-volume-service-credhub.yml"
       check_interpolation "enable-oci-phase-1.yml"
       check_interpolation "enable-routing-integrity.yml"
       check_interpolation "enable-smb-volume-service.yml"
@@ -36,6 +37,7 @@ test_experimental_ops() {
 
       check_interpolation "infrastructure-metrics.yml"
       check_interpolation "migrate-cf-mysql-to-pxc.yml"
+      check_interpolation "name: migrate-nfsbroker-mysql-to-credhub.yml" "${home}/operations/enable-nfs-volume-service.yml" "-o migrate-nfsbroker-mysql-to-credhub.yml" -l "${home}/operations/example-vars-files/vars-migrate-nfsbroker-mysql-to-credhub.yml"
 
       check_interpolation "name: perm-service.yml" "enable-mysql-tls.yml" "-o perm-service.yml -v perm_uaa_clients_cc_perm_secret=perm_secret -v perm_uaa_clients_perm_monitor_secret=perm_monitor_secret"
       check_interpolation "name: perm-service-with-pxc-release.yml" "perm-service.yml" "-o use-pxc.yml" "-o perm-service-with-pxc-release.yml -v perm_uaa_clients_cc_perm_secret=perm_secret -v perm_uaa_clients_perm_monitor_secret=perm_monitor_secret"
