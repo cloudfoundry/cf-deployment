85e839c0b267e06cc1a4ced25aca4bc52348eb29
Dave Walter <dwalter@pivotal.io>
2019-02-22 13:49:42 -0800

    Promote enable-smb-volume-service ops-file to GA
    [#163770401](https://www.pivotaltracker.com/story/show/163770401)

--- scripts/test-operations.sh
+++ scripts/test-operations.sh
@@ -28,6 +28,7 @@ test_standard_ops() {
       check_interpolation "enable-privileged-container-support.yml"
       check_interpolation "enable-routing-integrity.yml"
       check_interpolation "enable-service-discovery.yml"
+      check_interpolation "enable-smb-volume-service.yml"
 
       check_interpolation "migrate-cf-mysql-to-pxc.yml"
 
