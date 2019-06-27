4e961538caaa5c031afa0f83416bc7c0a5236089
Maria Shaldibina <mshaldibina@pivotal.io>
2019-06-27 10:30:56 -0400

    Add missing coverage for the new opsfile
    [#166882706](https://www.pivotaltracker.com/story/show/166882706)
    
    Co-authored-by: Amin Jamali <ajamali@pivotal.io>

--- units/tests/standard_test/operations_test.go
+++ units/tests/standard_test/operations_test.go
@@ -29,6 +29,7 @@ var standardTests = map[string]helpers.OpsFileTestParams{
 	"enable-routing-integrity.yml":            {},
 	"enable-service-discovery.yml":            {},
 	"enable-smb-volume-service.yml":           {},
+	"enable-tls-on-file-server.yml":           {},
 	"migrate-cf-mysql-to-pxc.yml":             {},
 	"openstack.yml":                           {},
 	"override-app-domains.yml": {
