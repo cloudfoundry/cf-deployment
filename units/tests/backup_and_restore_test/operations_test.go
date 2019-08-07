fe854f18e17a4e8c3c6d27ddec2e906ee5432de8
Jake Klein <jklein@pivotal.io>
2019-08-07 13:58:14 +0100

    reorder tests

--- units/tests/backup_and_restore_test/operations_test.go
+++ units/tests/backup_and_restore_test/operations_test.go
@@ -9,17 +9,20 @@ import (
 const testDirectory = "operations/backup-and-restore"
 
 var backupAndRestoreTests = map[string]helpers.OpsFileTestParams{
+	"enable-backup-restore.yml": {
+		Ops: []string{"enable-backup-restore.yml"},
+	},
 	"enable-backup-restore-azure.yml": {
 		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-azure-storage-blobstore.yml", "enable-backup-restore-azure.yml"},
 		VarsFiles: []string{"../example-vars-files/vars-use-azure-storage-blobstore.yml"},
 	},
+	"enable-restore-azure-clone.yml": {
+		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-azure-storage-blobstore.yml", "enable-restore-azure-clone.yml"},
+		VarsFiles: []string{"../example-vars-files/vars-use-azure-storage-blobstore.yml", "example-vars-files/vars-enable-restore-azure-clone.yml"},
+	},
 	"enable-backup-restore-gcs.yml": {
 		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-gcs-blobstore-service-account.yml", "enable-backup-restore-gcs.yml"},
 		VarsFiles: []string{"../example-vars-files/vars-use-gcs-blobstore-service-account.yml", "example-vars-files/vars-enable-backup-restore-gcs.yml"},
-	},
-	"enable-backup-restore-nfs-broker.yml": {
-		Ops:  []string{"enable-backup-restore.yml", "enable-backup-restore-nfs-broker.yml"},
-		Vars: []string{"nfs-broker-database-password=i_am_a_password"},
 	},
 	"enable-backup-restore-s3-unversioned.yml": {
 		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-s3-blobstore.yml", "enable-backup-restore-s3-unversioned.yml"},
@@ -29,16 +32,15 @@ var backupAndRestoreTests = map[string]helpers.OpsFileTestParams{
 		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-s3-blobstore.yml", "enable-backup-restore-s3-versioned.yml"},
 		VarsFiles: []string{"../example-vars-files/vars-use-s3-blobstore.yml"},
 	},
-	"enable-backup-restore.yml": {},
-	"enable-restore-azure-clone.yml": {
-		Ops:       []string{"enable-backup-restore.yml", "../use-external-blobstore.yml", "../use-azure-storage-blobstore.yml", "enable-restore-azure-clone.yml"},
-		VarsFiles: []string{"../example-vars-files/vars-use-azure-storage-blobstore.yml", "example-vars-files/vars-enable-restore-azure-clone.yml"},
-	},
 	"skip-backup-restore-droplets.yml": {
 		Ops: []string{"enable-backup-restore.yml", "skip-backup-restore-droplets.yml"},
 	},
 	"skip-backup-restore-droplets-and-packages.yml": {
 		Ops: []string{"enable-backup-restore.yml", "skip-backup-restore-droplets-and-packages.yml"},
+	},
+	"enable-backup-restore-nfs-broker.yml": {
+		Ops:  []string{"enable-backup-restore.yml", "enable-backup-restore-nfs-broker.yml"},
+		Vars: []string{"nfs-broker-database-password=i_am_a_password"},
 	},
 }
 
