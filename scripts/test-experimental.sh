543adb786af6b95270e63c111df0760a998c4a4b
Dave Walter <dwalter@pivotal.io>
2019-09-05 11:27:45 -0700

    Remove deprecated ops-file: perm-service-with-pxc-release.yml
    [#167954290](https://www.pivotaltracker.com/story/show/167954290)

--- scripts/test-experimental.sh
+++ scripts/test-experimental.sh
@@ -29,8 +29,7 @@ test_experimental_ops() {
 
       check_interpolation "infrastructure-metrics.yml"
 
-      check_interpolation "name: perm-service-with-pxc-release.yml" "perm-service-with-pxc-release.yml -v perm_uaa_clients_cc_perm_secret=perm_secret -v perm_uaa_clients_perm_monitor_secret=perm_monitor_secret"
-      check_interpolation "name: perm-service-with-tcp-routing.yml" "perm-service-with-pxc-release.yml -v perm_uaa_clients_cc_perm_secret=perm_secret -v perm_uaa_clients_perm_monitor_secret=perm_monitor_secret" "-o perm-service-with-tcp-routing.yml"
+      check_interpolation "name: perm-service-with-tcp-routing.yml" -v perm_uaa_clients_cc_perm_secret=perm_secret -v perm_uaa_clients_perm_monitor_secret=perm_monitor_secret" "-o perm-service-with-tcp-routing.yml"
 
       check_interpolation "rootless-containers.yml"
 
